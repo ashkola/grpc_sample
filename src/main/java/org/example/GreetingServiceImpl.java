62bc6b04c19dcf70a51ec4846c0f8d0978abadf6
ashkola <ashkola@gmail.com>
2021-03-09 02:49:45 +0300

    1

---
+++ src/main/java/org/example/GreetingServiceImpl.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+import io.grpc.stub.StreamObserver;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest request, StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+        System.out.println(request);
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+            GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder().setGreeting("Hello from server, " + request.getName()).build();
+            responseObserver.onNext(response);
+        }
+        responseObserver.onCompleted();
+    }
+}
